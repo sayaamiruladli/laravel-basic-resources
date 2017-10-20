a3b6e8d5a3317b86b1fd9b0759121c33f3105843
Amirul Adli <sayaamiruladli@gmail.com>
2017-10-20 15:21:56 +0800

    add submodule

---
+++ views/pages/spm.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.master')
+
+@section('content')
+
+<section class="hero bg-rainbow">
+  <div class="hero-body">
+    <div class="container">
+      <h1 class="title">
+        Primary title
+      </h1>
+      <h2 class="subtitle">
+        Primary subtitle
+      </h2>
+    </div>
+  </div>
+</section>
+
+@endsection
